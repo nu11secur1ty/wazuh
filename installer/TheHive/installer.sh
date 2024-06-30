b34206d5730cdf823f2c5224521f3cf45283fe97
nu11secur1ty <venvaropt@gmail.com>
2024-06-30 11:47:51 +0300

    Update installer.sh

--- installer/TheHive/installer.sh
+++ installer/TheHive/installer.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
 curl -s https://raw.githubusercontent.com/nu11secur1ty/Docker/main/installer_24.04.sh | sh
+git clone https://github.com/f0rc3ps/TheHive.git
+cd TheHive
 docker-compose up -d
